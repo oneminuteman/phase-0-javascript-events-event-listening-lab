f8e8ef703898e636c30d3335541e73ae8300dd14
Victor <victor.ireri@student.moringaschool.com>
2023-03-25 14:15:50 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const button = document.getElementById("myButton");
+button.addEventListener("click", function(doMath) {
+  console.log("Button clicked!");
+})
+
+let a = 3
+let b = 4
+ 
+function doMath (a, b);
+ let result =  a * b
+return result ;
+
